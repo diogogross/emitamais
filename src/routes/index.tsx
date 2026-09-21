c5192680157ee90761cb35fc42127e25ba6f4a00
diogogross <dalprapf1@gmail.com>
2026-09-21 17:57:20 -0300

    fix: register home page as TanStack route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import { createFileRoute } from '@tanstack/react-router';
 
-export default function Index() {
+export const Route = createFileRoute('/')({
+  component: Index,
+});
+
+function Index() {
   return (
     <div className="p-4">
       <h1>Bem-vindo ao Emita Go</h1>
